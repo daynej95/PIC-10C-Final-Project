2a46d055ad90af67d2063be3c69e29b698d8f6fa
daynej95 <daynej95@gmail.com>
2017-03-25 20:30:49 -0700

    Added member functions to Number.h
    Implemented and defined:
    Default constructor
    Constructor that takes an int as an argument
    is_prime(), which returns true if the Number is prime

--- Number.h
+++ Number.h
@@ -1,16 +1,45 @@
+#include <iostream>
+
 class Number {
 
 private:
 
 	int val;
+	bool prime;
 
 
 
 public:
 
+	//Default constructor
+	Number() : val(1), prime(false){}
+
+
+	//Constructor that takes an int to use for the Number's value (must be a positive int)
+	Number(const int& num) : val(num), prime(false){
+		if (num < 1){
+			std::cout << "Warning: All numbers must be positive." << std::endl;
+			throw;
+		}
+		prime = this->is_prime();
+	}
+
 	//returns true if val is divisible by rhs
-	bool is_div(int rhs){
+	bool is_div(const int& rhs){
 		return val % rhs == 0;
 	}
+
+	//returns true if val is a prime number
+	bool is_prime(){
+		for (int i = 2; i < val; i++){
+			if (this->is_div(i)){
+				return false;
+			}
+			return true;
+		}
+	}
+
+
+
 
 };
